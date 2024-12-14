34994bdbdbc3de6b3a24778ada31755b932b1275
Анастасия <azizovaa031@gmail.com>
2024-12-14 23:39:32 +0700

    внесены зависимости

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,17 +33,23 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
 }
 
 dependencies {
-    
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation("androidx.room:room-runtime:2.x.x")
-    //kapt("androidx.room:room-compiler:2.x.x")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.5.1")
+    implementation("androidx.room:room-runtime:2.4.2")
+    //implementation("org.jetbrains.kotlin:kotlin-stdlib:$kotlin_version")
+    implementation("androidx.appcompat:appcompat:1.4.1")
+    implementation("androidx.core:core-ktx:1.7.0")
+    implementation("com.google.android.material:material:1.5.0")
+    implementation("androidx.recyclerview:recyclerview:1.2.1")
+    //kapt("androidx.room:room-compiler:2.4.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.1")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.4.1")
     implementation("androidx.work:work-runtime-ktx:2.7.0")
     implementation("com.google.code.gson:gson:2.8.5")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
@@ -52,6 +58,9 @@ dependencies {
     implementation("androidx.recyclerview:recyclerview:1.3.2")
 
     testImplementation(libs.junit)
+    testImplementation("junit:junit:4.13.2")//
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation("androidx.test.ext:junit:1.1.3")//
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")//
 }
