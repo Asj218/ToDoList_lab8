0e0bf9731d9126329a41df170d98cec0181fe100
Анастасия <azizovaa031@gmail.com>
2024-12-15 00:12:37 +0700

    изменены данные для модели TaskDao.kt

--- app/src/main/java/com/bignerdranch/android/todolist/TaskDao.kt
+++ app/src/main/java/com/bignerdranch/android/todolist/TaskDao.kt
@@ -1,18 +1,19 @@
 package com.bignerdranch.android.todolist
 
 import androidx.room.Dao
-import androidx.room.Delete
 import androidx.room.Insert
-import retrofit2.http.Query
+import androidx.room.Query
+import androidx.room.Delete
 
 @Dao
 interface TaskDao {
     @Insert
     suspend fun insert(task: Task)
 
-    @Delete
-    suspend fun delete(task: Task)
-
     @Query("SELECT * FROM tasks ORDER BY priority ASC")
     suspend fun getAllTasks(): List<Task>
+
+    @Delete
+    suspend fun delete(task: Task)
 }
+
