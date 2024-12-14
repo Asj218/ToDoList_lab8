be074f488df775c3fc38e65891f64fee783c2184
Анастасия <azizovaa031@gmail.com>
2024-12-14 22:59:20 +0700

    создала класс для активности ListActivity.kt:

---
+++ app/src/main/java/com/bignerdranch/android/todolist/ListActivity.kt
@@ -0,0 +1,7 @@
+package com.bignerdranch.android.todolist
+
+import androidx.appcompat.app.AppCompatActivity
+
+class ListActivity : AppCompatActivity() {
+    // Код для отображения списка задач и обработки FAB
+}
