03fb2bbbd22968bd1eba04e2274a77d69c20777d
Анастасия <azizovaa031@gmail.com>
2024-12-15 00:11:58 +0700

    изменены данные для модели Task.kt

--- app/src/main/java/com/bignerdranch/android/todolist/Task.kt
+++ app/src/main/java/com/bignerdranch/android/todolist/Task.kt
@@ -3,11 +3,9 @@ package com.bignerdranch.android.todolist
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-interface Task {
-    @Entity(tableName = "tasks")
-    data class Task(
-        @PrimaryKey(autoGenerate = true) val id: Long = 0,
-        val description: String,
-        val priority: Int // 1 - High, 2 - Medium, 3 - Low
-    )
-}
+@Entity(tableName = "tasks")
+data class Task(
+    @PrimaryKey(autoGenerate = true) val id: Long = 0,
+    val description: String,
+    val priority: Int
+)
