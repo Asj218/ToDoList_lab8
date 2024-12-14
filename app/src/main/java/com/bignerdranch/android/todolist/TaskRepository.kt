ef6e069accaf95204c24311d8724eddcdeb1a056
Анастасия <azizovaa031@gmail.com>
2024-12-15 01:17:08 +0700

    исправлены ошибки в TaskRepository.kt и TaskViewModel.kt

--- app/src/main/java/com/bignerdranch/android/todolist/TaskRepository.kt
+++ app/src/main/java/com/bignerdranch/android/todolist/TaskRepository.kt
@@ -3,7 +3,7 @@ package com.bignerdranch.android.todolist
 import androidx.lifecycle.LiveData
 
 class TaskRepository(private val taskDao: TaskDao) {
-    val allTasks: LiveData<List<Task>> = taskDao.getAllTasks()
+    val allTasks: LiveData<List<Task>> = taskDao.getAllTasks() // Теперь работает корректно
 
     suspend fun insert(task: Task) {
         taskDao.insert(task)
--- app/src/main/java/com/bignerdranch/android/todolist/TaskViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/todolist/TaskViewModel.kt
@@ -5,7 +5,7 @@ import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
 
 class TaskViewModel(private val repository: TaskRepository) : ViewModel() {
-    val allTasks = repository.getAllTasks()
+    val allTasks = repository.allTasks // Теперь корректно с LiveData
 
     fun insert(task: Task) {
         viewModelScope.launch {
