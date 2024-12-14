44b9ed1e598d058a455830f148e806eb7c0a3227
Анастасия <azizovaa031@gmail.com>
2024-12-14 23:01:50 +0700

    создала адаптер для RecyclerView TaskAdapter

---
+++ app/src/main/java/com/bignerdranch/android/todolist/TaskAdapter.kt
@@ -0,0 +1,7 @@
+package com.bignerdranch.android.todolist
+
+import androidx.recyclerview.widget.RecyclerView
+
+class TaskAdapter(private val tasks: List<Task>) : RecyclerView.Adapter<TaskAdapter.TaskViewHolder>() {
+    // Код для адаптера
+}
